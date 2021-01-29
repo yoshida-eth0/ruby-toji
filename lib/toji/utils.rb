a5960dc74b1f0d7b769b6af1638bc3907c847dd7
yoshida <yoshida.eth0@gmail.com>
2021-01-29 23:32:37 +0900

    mod Utils.check_dup

--- lib/toji/utils.rb
+++ lib/toji/utils.rb
@@ -1,14 +1,12 @@
 module Toji
   module Utils
-    def self.activerecord_defined?
-      Object.const_defined?(:ActiveRecord) && ActiveRecord.const_defined?(:Base)
+    def self.activemodel_defined?
+      Object.const_defined?(:ActiveModel) && ActiveModel.const_defined?(:Validations)
     end
 
     def self.check_dup(obj)
-      if activerecord_defined? && ActiveRecord::Base===obj
-        if !obj.class.method_defined?(:initialize_dup, false)
-          raise Error, "implementation required: #{obj.class}.initialize_dup"
-        end
+      if activemodel_defined? && ActiveModel::Validations===obj
+        # ok
       else
         if !obj.class.private_method_defined?(:initialize_copy, false)
           raise Error, "implementation required: #{obj.class}.initialize_copy"
